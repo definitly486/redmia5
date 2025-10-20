f44450419d19bbe0b7f7f6f68e19488e8faca563
Your Name <you@example.com>
2025-10-20 18:49:36 +0300

    изменено:      unistall_pkg 	новый файл:    ../release_apk.sh

---
+++ release_apk.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+APK_PATH="$HOME/AndroidStudioProjects/App_Redmi_A5/app/build/outputs/apk/debug"
+cd $HOME/redmia5
+mv $APK_PATH/app-debug.apk  $APK_PATH/app_redmi_a5.apk
+echo "Y" | gh release  delete-asset apk app_redmi_a5.apk
+gh release  upload apk $APK_PATH/app_redmi_a5.apk
