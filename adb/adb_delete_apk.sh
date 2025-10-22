77ac83f813fe0747ddf8468fa472f6023d322bcb
Your Name <you@example.com>
2025-10-22 20:25:57 +0300

    изменено:      adb/adb_delete_apk.sh 	изменено:      adb/unistall_pkg 	новый файл:    note

--- adb/adb_delete_apk.sh
+++ adb/adb_delete_apk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 killall -9 adb
 dir=$(dirname "$(realpath $0)")
-adb shell su - root -c "mount -o rw,remount /"
+adb shell su - root -c "remount"
   for i in $(cat $dir/pkg_path ); do
 
     adb shell        su - root -c  "rm -r    $i" 
