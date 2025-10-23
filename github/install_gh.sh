69457271b364eabbc496ae148b1d43b9fa394dee
Your Name <you@example.com>
2025-10-23 20:35:50 +0300

    новый файл:    github/install_gh.sh

---
+++ github/install_gh.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+
+if [ -z "$1" ]
+then
+     echo "не введен пароль для gh"
+     exit
+fi
+
+
+
+if ! [  -f "gh.tar.xz.enc" ]; then
+     curl -L -o   gh.tar.xz.enc  https://github.com/definitly486/definitly486/releases/download/gh/gh.tar.xz.enc
+fi
+
+
+openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e  -d  -in gh.tar.xz.enc  -out gh.tar.xz -pass pass:$1
+tar xf  gh.tar.xz
+mkdir -p $HOME/.config
+cp -r gh $HOME/.config
