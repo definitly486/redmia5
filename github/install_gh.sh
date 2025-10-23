e02b2106677de0b5197b3cb3aff2451f0426bbf3
Your Name <you@example.com>
2025-10-23 20:43:54 +0300

    изменено:      github/install_gh.sh

--- github/install_gh.sh
+++ github/install_gh.sh
@@ -9,11 +9,11 @@ fi
 
 
 if ! [  -f "gh.tar.xz.enc" ]; then
-     curl -L -o   gh.tar.xz.enc  https://github.com/definitly486/definitly486/releases/download/gh/gh.tar.xz.enc
+  busybox  curl -L -o   gh.tar.xz.enc  https://github.com/definitly486/definitly486/releases/download/gh/gh.tar.xz.enc
 fi
 
 
 openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e  -d  -in gh.tar.xz.enc  -out gh.tar.xz -pass pass:$1
-tar xf  gh.tar.xz
+busybox tar xf  gh.tar.xz
 mkdir -p $HOME/.config
 cp -r gh $HOME/.config
