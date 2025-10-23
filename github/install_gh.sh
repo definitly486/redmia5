40f216f576b4a18b8850e9ac4f6710c73cfacf31
Your Name <you@example.com>
2025-10-23 20:58:24 +0300

    изменено:      github/install_gh.sh

--- github/install_gh.sh
+++ github/install_gh.sh
@@ -9,7 +9,7 @@ fi
 
 
 if ! [  -f "gh.tar.xz.enc" ]; then
-  busybox  wget  -O   gh.tar.xz.enc  https://github.com/definitly486/definitly486/releases/download/gh/gh.tar.xz.enc
+  busybox  wget --no-check-certificate  -O   gh.tar.xz.enc  https://github.com/definitly486/definitly486/releases/download/gh/gh.tar.xz.enc
 fi
 
 
