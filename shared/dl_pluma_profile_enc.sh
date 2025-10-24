4b4c1bd583c4bd123d7487d5a860f27de433b0d2
Your Name <you@example.com>
2025-10-24 17:40:46 +0300

    изменено:      shared/dl_pluma_profile_enc.sh

--- shared/dl_pluma_profile_enc.sh
+++ shared/dl_pluma_profile_enc.sh
@@ -14,7 +14,7 @@ if ! [  -f "com.qflair.browserq.tar.xz.enc" ]; then
      curl -k  -L -o    com.qflair.browserq.tar.xz.enc    https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/com.qflair.browserq.tar.xz.enc 
 fi
 openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d  -in   com.qflair.browserq.tar.xz.enc -out  com.qflair.browserq.tar.xz     -pass pass:$1   
-tar xf com.qflair.browserq.tar.xz
+busybox tar xf com.qflair.browserq.tar.xz
 su - root -c "mkdir -p /data/data/com.qflair.browserq"
 su - root -c "cp -r  $DIR/com.qflair.browserq/databases   /data/data/com.qflair.browserq"
 su - root -c "chmod -R  700  /data/data/com.qflair.browserq/databases"
