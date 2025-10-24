d6a9d0a0f96241aaec724b7234e417b39186884a
Your Name <you@example.com>
2025-10-24 20:23:42 +0300

    изменено:      shared/dl_telegram_profile.sh

--- shared/dl_telegram_profile.sh
+++ shared/dl_telegram_profile.sh
@@ -1,20 +1,32 @@
-#!/system/bin/sh
-if [ -z "$1" ]
-then
-     echo "не введен пароль для распаковки telegram "
-     exit
+#!/bin/bash
+
+# Проверяем наличие введенного пароля
+if [ -z "$1" ]; then
+    echo "не введен пароль для распаковки telegram"
+    exit 1
 fi
 
-if ! [  -f "td.binlog.tar.xz.enc" ]; then
-     curl -k -L -o   td.binlog.tar.xz.enc    https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/td.binlog.tar.xz.enc 
+PASSWORD="$1" # сохраняем пароль в отдельной переменной
+
+# Скачиваем зашифрованный архив, если его ещё нет
+if ! [ -f "td.binlog.tar.xz.enc" ]; then
+    curl -k -L -o td.binlog.tar.xz.enc \
+        https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/td.binlog.tar.xz.enc
 fi
 
-openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d  -in  td.binlog.tar.xz.enc  -out  td.binlog.tar.xz     -pass pass:$1   
-busybox tar xf td.binlog.tar.xz 
-ID=$(su - root -c "ls  -l /data/data/ | grep chall | head -n 1 | sed 's/[^ ]* //' |  cut -d' ' -f1")
-ID2=$(echo $ID | tr -d '\r')
-su - root -c "chown $ID2:$ID2 td.binlog"
-su - root -c "rm  /data/data/org.thunderdog.challegram/files/tdlib/td.binlog"
-su - $ID2  -c "cp  td.binlog /data/data/org.thunderdog.challegram/files/tdlib"
+# Используем экранирование пароля с помощью printf
+openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -d \
+    -in td.binlog.tar.xz.enc \
+    -out td.binlog.tar.xz \
+    -pass pass:"$(printf '%q' "$PASSWORD")"
 
+# Извлекаем содержимое архива
+busybox tar xf td.binlog.tar.xz
 
+# Определяем ID пользователя приложения Challegram
+ID=$(su - root -c "ls -l /data/data/ | grep chall | head -n 1 | awk '{print \$NF}'")
+
+# Удаляем старый файл журнала TDLib и копируем новый
+su - root -c "chown $ID:$ID td.binlog"
+su - root -c "rm /data/data/org.thunderdog.challegram/files/tdlib/td.binlog"
+su - $ID -c "cp td.binlog /data/data/org.thunderdog.challegram/files/tdlib/"
