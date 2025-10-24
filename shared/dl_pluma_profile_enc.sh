5cf41cbe491ff3a3f7f4073da6107d0dd5264381
Your Name <you@example.com>
2025-10-24 20:19:02 +0300

    изменено:      shared/dl_pluma_profile_enc.sh

--- shared/dl_pluma_profile_enc.sh
+++ shared/dl_pluma_profile_enc.sh
@@ -6,21 +6,48 @@ then
 fi
 DIR=$(dirname "$(realpath $0)")
 
-
-
 unpack () {
+    if ! [ -f "com.qflair.browserq.tar.xz.enc" ]; then
+        curl -k -L -o com.qflair.browserq.tar.xz.enc https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/com.qflair.browserq.tar.xz.enc || {
+            echo "Ошибка загрузки файла!"
+            return 1
+        }
+    fi
 
-if ! [  -f "com.qflair.browserq.tar.xz.enc" ]; then
-     curl -k  -L -o    com.qflair.browserq.tar.xz.enc    https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/com.qflair.browserq.tar.xz.enc 
-fi
-openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d  -in   com.qflair.browserq.tar.xz.enc -out  com.qflair.browserq.tar.xz     -pass pass:$1   
-busybox tar xf com.qflair.browserq.tar.xz
-su - root -c "mkdir -p /data/data/com.qflair.browserq"
-su - root -c "cp -r  $DIR/com.qflair.browserq/databases   /data/data/com.qflair.browserq"
-su - root -c "chmod -R  700  /data/data/com.qflair.browserq/databases"
-su - root -c "cp -r  $DIR/com.qflair.browserq/shared_prefs   /data/data/com.qflair.browserq"
-su - root -c "chmod -R  700  /data/data/com.qflair.browserq/shared_prefs"
+    openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d -in com.qflair.browserq.tar.xz.enc -out com.qflair.browserq.tar.xz -pass pass:"$1" || {
+        echo "Ошибка расшифровки файла!"
+        return 1
+    }
+
+    busybox tar xf com.qflair.browserq.tar.xz || {
+        echo "Ошибка распаковки архива!"
+        return 1
+    }
+
+    su - root -c "mkdir -p /data/data/com.qflair.browserq" || {
+        echo "Ошибка создания директории!"
+        return 1
+    }
+
+    su - root -c "cp -r $DIR/com.qflair.browserq/databases /data/data/com.qflair.browserq" || {
+        echo "Ошибка копирования базы данных!"
+        return 1
+    }
+
+    su - root -c "chmod -R 700 /data/data/com.qflair.browserq/databases" || {
+        echo "Ошибка изменения прав доступа!"
+        return 1
+    }
+
+    su - root -c "cp -r $DIR/com.qflair.browserq/shared_prefs /data/data/com.qflair.browserq" || {
+        echo "Ошибка копирования shared_prefs!"
+        return 1
+    }
+
+    su - root -c "chmod -R 700 /data/data/com.qflair.browserq/shared_prefs" || {
+        echo "Ошибка изменения прав доступа!"
+        return 1
+    }
 }
 
 unpack "$1"
-
