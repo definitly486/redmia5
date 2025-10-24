2414d6c2ffe5af129605f94d9a60df2a466d7193
Your Name <you@example.com>
2025-10-24 17:48:05 +0300

    изменено:      shared/dl_pluma_profile.sh

--- shared/dl_pluma_profile.sh
+++ shared/dl_pluma_profile.sh
@@ -3,7 +3,7 @@ DIR=$(dirname "$(realpath $0)")
 if ! [  -f "com.qflair.browserq.tar.xz" ]; then
      curl -k -L -o   com.qflair.browserq.tar.xz   https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/com.qflair.browserq.tar.xz
 fi
-busy tar xf com.qflair.browserq.tar.xz
+busybox tar xf com.qflair.browserq.tar.xz
 su - root -c "mkdir -p /data/data/com.qflair.browserq"
 su - root -c "cp -r  $DIR/com.qflair.browserq/databases   /data/data/com.qflair.browserq"
 su - root -c "chmod -R  700  /data/data/com.qflair.browserq/databases"
