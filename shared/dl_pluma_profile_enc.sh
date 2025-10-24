39079fe3641ce3311998bc1c6adf4740089ceed2
Your Name <you@example.com>
2025-10-24 17:33:56 +0300

    новый файл:    shared/dl_pluma_profile_enc.sh

---
+++ shared/dl_pluma_profile_enc.sh
@@ -0,0 +1,26 @@
+#!/system/bin/sh
+if [ -z "$1" ]
+then
+     echo "не введен пароль для распаковки pluma "
+     exit
+fi
+DIR=$(dirname "$(realpath $0)")
+
+
+
+unpack () {
+
+if ! [  -f "com.qflair.browserq.tar.xz.enc" ]; then
+     curl -k  -L -o    com.qflair.browserq.tar.xz.enc    https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/com.qflair.browserq.tar.xz.enc 
+fi
+openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d  -in   com.qflair.browserq.tar.xz.enc -out  com.qflair.browserq.tar.xz     -pass pass:$1   
+tar xf com.qflair.browserq.tar.xz
+su - root -c "mkdir -p /data/data/com.qflair.browserq"
+su - root -c "cp -r  $DIR/com.qflair.browserq/databases   /data/data/com.qflair.browserq"
+su - root -c "chmod -R  700  /data/data/com.qflair.browserq/databases"
+su - root -c "cp -r  $DIR/com.qflair.browserq/shared_prefs   /data/data/com.qflair.browserq"
+su - root -c "chmod -R  700  /data/data/com.qflair.browserq/shared_prefs"
+}
+
+ unpack 
+
