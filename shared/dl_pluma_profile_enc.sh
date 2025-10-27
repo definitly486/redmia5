1dd5c4b08eb4c8195075adaf6984f4bd05cc7bba
Your Name <you@example.com>
2025-10-27 23:39:57 +0300

    изменено:      shared/dl_pluma_profile_enc.sh

--- shared/dl_pluma_profile_enc.sh
+++ shared/dl_pluma_profile_enc.sh
@@ -1,53 +1,32 @@
 #!/system/bin/sh
-if [ -z "$1" ]
-then
-     echo "не введен пароль для распаковки pluma "
-     exit
+
+# Проверяем наличие введенного пароля
+if [ -z "$1" ]; then
+    echo "не введен пароль для распаковки telegram"
+    exit 1
 fi
-DIR=$(dirname "$(realpath $0)")
 
-unpack () {
-    if ! [ -f "com.qflair.browserq.tar.xz.enc" ]; then
-        curl -k -L -o com.qflair.browserq.tar.xz.enc https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/shared/com.qflair.browserq.tar.xz.enc || {
-            echo "Ошибка загрузки файла!"
-            return 1
-        }
-    fi
+PASSWORD="$1" # сохраняем пароль в отдельной переменной
 
-    openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -e -d -in com.qflair.browserq.tar.xz.enc -out com.qflair.browserq.tar.xz -pass pass:"$1" || {
-        echo "Ошибка расшифровки файла!"
-        return 1
-    }
+# Скачиваем зашифрованный архив, если его ещё нет
+if ! [ -f "com.qflair.browserq.tar.enc" ]; then
+    curl -k -L -o com.qflair.browserq.tar.enc \
+       https://github.com/definitly486/redmia5/releases/download/shared/com.qflair.browserq.tar.enc
+fi
 
-    busybox tar xf com.qflair.browserq.tar.xz || {
-        echo "Ошибка распаковки архива!"
-        return 1
-    }
+# Используем экранирование пароля с помощью printf
+openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -d \
+    -in com.qflair.browserq.tar.enc  \
+    -out com.qflair.browserq.tar \
+    -pass pass:$PASSWORD
 
-    su - root -c "mkdir -p /data/data/com.qflair.browserq" || {
-        echo "Ошибка создания директории!"
-        return 1
-    }
+# Извлекаем содержимое архива
+su - root -c "rm -R com.qflair.browserq"
+busybox tar xf com.qflair.browserq.tar
 
-    su - root -c "cp -r $DIR/com.qflair.browserq/databases /data/data/com.qflair.browserq" || {
-        echo "Ошибка копирования базы данных!"
-        return 1
-    }
+# Определяем ID пользователя приложения com.qflair.browserq
+ID=$(su - root -c "ls -l /data/data/ | grep qflair | head -n 1 | awk '{print \$3}'")
 
-    su - root -c "chmod -R 700 /data/data/com.qflair.browserq/databases" || {
-        echo "Ошибка изменения прав доступа!"
-        return 1
-    }
-
-    su - root -c "cp -r $DIR/com.qflair.browserq/shared_prefs /data/data/com.qflair.browserq" || {
-        echo "Ошибка копирования shared_prefs!"
-        return 1
-    }
-
-    su - root -c "chmod -R 700 /data/data/com.qflair.browserq/shared_prefs" || {
-        echo "Ошибка изменения прав доступа!"
-        return 1
-    }
-}
-
-unpack "$1"
+# Удаляем старый файл журнала TDLib и копируем новый
+su - root -c "cp -R  com.qflair.browserq  /data/data/"
+su - root -c "chown -R  $ID:$ID /data/data/com.qflair.browserq"
