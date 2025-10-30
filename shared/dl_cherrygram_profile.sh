c9ebee9dd99cf83dde06215345e8e1a9ff1264e0
Your Name <you@example.com>
2025-10-31 01:43:06 +0300

    изменено:      shared/dl_cherrygram_profile.sh

--- shared/dl_cherrygram_profile.sh
+++ shared/dl_cherrygram_profile.sh
@@ -18,10 +18,10 @@ fi
 openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -d \
     -in  uz.unnarsx.cherrygram.tar.enc \
     -out uz.unnarsx.cherrygram.tar \
-    -pass pass:$PASSWORD
+    -pass pass:"$PASSWORD"
 
 # Извлекаем содержимое архива
-su - root -c "rm -R "uz.unnarsx.cherrygram"
+su - root -c "rm -R uz.unnarsx.cherrygram"
 busybox tar xf uz.unnarsx.cherrygram.tar
 
 # Определяем ID пользователя приложения Challegram
@@ -29,5 +29,5 @@ ID=$(su - root -c "ls -l /data/data/ | grep cherr | head -n 1 | awk '{print \$3}
 
 # Удаляем старый файл журнала TDLib и копируем новый
 su - root -c "rm /data/data/org.thunderdog.challegram/files/tdlib/td.binlog"
-su - root -c "cp -R  uz.unnarsx.cherrygram  /data/data/"
-su - root -c "chown -R  $ID:$ID /data/data/uz.unnarsx.cherrygram"
+su - root -c "cp -R uz.unnarsx.cherrygram /data/data/"
+su - root -c "chown -R \"$ID\":$ID /data/data/uz.unnarsx.cherrygram"
