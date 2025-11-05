b613f6cdb2d7a1888ed801d766b6f7e1d7893cb8
Your Name <you@example.com>
2025-11-05 10:53:47 +0300

    новый файл:    shared/dl_googleauth_profile_enc.sh

---
+++ shared/dl_googleauth_profile_enc.sh
@@ -0,0 +1,32 @@
+#!/system/bin/sh
+
+# Проверяем наличие введенного пароля
+if [ -z "$1" ]; then
+    echo "не введен пароль для распаковки telegram"
+    exit 1
+fi
+
+PASSWORD="$1" # сохраняем пароль в отдельной переменной
+
+# Скачиваем зашифрованный архив, если его ещё нет
+if ! [ -f "com.google.android.apps.authenticator2.tar.enc" ]; then
+    curl -k -L -o com.google.android.apps.authenticator2.tar.enc \
+       https://github.com/definitly486/redmia5/releases/download/shared/com.google.android.apps.authenticator2.tar.enc
+fi
+
+# Используем экранирование пароля с помощью printf
+openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -d \
+    -in com.google.android.apps.authenticator2.tar.enc  \
+    -out com.google.android.apps.authenticator2.tar \
+    -pass pass:$PASSWORD
+
+# Извлекаем содержимое архива
+su - root -c "rm -R com.google.android.apps.authenticator2"
+busybox tar xf com.google.android.apps.authenticator2.tar
+
+# Определяем ID пользователя приложения com.qflair.browserq
+ID=$(su - root -c "ls -l /data/data/ | grep authenticator2 | head -n 1 | awk '{print \$3}'")
+
+# Удаляем старый файл журнала TDLib и копируем новый
+su - root -c "cp -R com.google.android.apps.authenticator2   /data/data/"
+su - root -c "chown -R  $ID:$ID /data/data/com.google.android.apps.authenticator2/"
