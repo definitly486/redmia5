117c8b83fe7e72178b0acf931dfabb302686a5e7
Your Name <you@example.com>
2025-11-05 15:59:22 +0300

    изменено:      github/install_git_core.sh

--- github/install_git_core.sh
+++ github/install_git_core.sh
@@ -8,3 +8,6 @@ fi
 busybox tar xf  git-core.tar
 su - root -c "mkdir -p /data/data/com.termux/files/usr/share"
 su - root -c "cp -r git-core   /data/data/com.termux/files/usr/share"
+
+ID=$(su - root -c "ls -l /data/data/ | grep termos | head -n 1 | awk '{print \$3}'")
+su - root -c "chown -R  $ID:$ID /data/data/com.termux"
