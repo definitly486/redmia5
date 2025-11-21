1d30679635331ff657e8fec84eba66b27f757854
Your Name <you@example.com>
2025-11-21 06:26:02 +0300

    изменено:      python3/install_python3.sh

--- python3/install_python3.sh
+++ python3/install_python3.sh
@@ -6,5 +6,6 @@ fi
 
 su - root -c "busybox tar xf python-3.13-android-aarch64.tar.gz  -C /data/local/tmp"
 su - root -c "chmod -R 0755 /data/local/tmp/python-android-aarch64"
+su - root -c "chmod +x /data/local/tmp/python-android-aarch64/bin/python3.13"
 su - root -c "cp python3 /system/bin"
 su - root -c "chmod -R 0755 /system/bin/python3"
