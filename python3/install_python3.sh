05184a8606014e48ae18ff7d1d17a684b20030c6
Your Name <you@example.com>
2025-11-21 06:22:10 +0300

    изменено:      python3/install_python3.sh 	новый файл:    python3/python3

--- python3/install_python3.sh
+++ python3/install_python3.sh
@@ -5,3 +5,6 @@ if ! [  -f "python-3.13-android-aarch64.tar.gz" ]; then
 fi
 
 su - root -c "busybox tar xf python-3.13-android-aarch64.tar.gz  -C /data/local/tmp"
+su - root -c "chmod -R 0755 /data/local/tmp/python-android-aarch64"
+su - root -c "cp python3 /system/bin"
+su - root -c "chmod -R 0755 /system/bin/python3"
