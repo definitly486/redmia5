8bfd900e917e1d9cc32811cff535a0ce0bb40444
Your Name <you@example.com>
2025-11-21 06:01:30 +0300

    новый файл:    python3/install_python3.sh

---
+++ python3/install_python3.sh
@@ -0,0 +1,7 @@
+#!/system/bin/sh
+
+if ! [  -f "python-3.13-android-aarch64.tar.gz" ]; then
+    curl -L -k  -o   python-3.13-android-aarch64.tar.gz  https://github.com/definitly486/redmia5/releases/download/python3/python-3.13-android-aarch64.tar.gz
+fi
+
+busybox tar xf  gh.tar.xz -C /data/local/tmp
