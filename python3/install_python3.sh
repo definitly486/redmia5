6034e7ef04e9502c17c798ee6dcb373abec2c124
Your Name <you@example.com>
2025-11-21 06:11:47 +0300

    изменено:      python3/install_python3.sh

--- python3/install_python3.sh
+++ python3/install_python3.sh
@@ -4,4 +4,4 @@ if ! [  -f "python-3.13-android-aarch64.tar.gz" ]; then
     curl -L -k  -o   python-3.13-android-aarch64.tar.gz  https://github.com/definitly486/redmia5/releases/download/python3/python-3.13-android-aarch64.tar.gz
 fi
 
-busybox tar xf python-3.13-android-aarch64.tar.gz  -C /data/local/tmp
+su - root -c "busybox tar xf python-3.13-android-aarch64.tar.gz  -C /data/local/tmp"
